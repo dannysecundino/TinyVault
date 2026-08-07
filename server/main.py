b1bebab5770adfa4312802f1ce0e72a71622ccfd
dannysecundino <dannyssfilho@gmail.com>
2026-08-07 11:50:07 -0300

    deletado o pycache

--- server/main.py
+++ server/main.py
@@ -5,19 +5,20 @@ import threading
 HOST = "127.0.0.1"
 PORT = 9090
 
-# banco de dados
-bd = db.pegar_json()
+if __name__ == "__main__":          # isso serve para que essa parte rode só se este arquivo for rodado como o principal
+    # banco de dados
+    bd = db.pegar_json()
 
-# socket de escuta
-lsock = net.criar_socket_escuta(HOST, PORT)
+    # socket de escuta
+    lsock = net.criar_socket_escuta(HOST, PORT)
 
-# laco principal
-while True:
-    # aceitar client
-    csock, client_addr = lsock.accept()
-    print(f">> server connected to {client_addr}")
+    # laco principal
+    while True:
+        # aceitar client
+        csock, client_addr = lsock.accept()
+        print(f">> server connected to {client_addr}")
 
-    # abrir uma thread
-    th = threading.Thread(target = net.atender_client, args=(csock, bd))
-    th.start()
-lsock.close()
+        # abrir uma thread
+        th = threading.Thread(target = net.atender_client, args=(csock, bd))
+        th.start()
+    lsock.close()
